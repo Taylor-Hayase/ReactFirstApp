a90372eca5dea3fd235d1e805a3e1d035b2df3f2
Taylor Hayase <thayase@calpoly.edu>
2020-04-15 12:29:40 -0700

    finished 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
 import Table from './Table'
 import Form from './Form'
+import axios from 'axios';
 
 class App extends Component {
   state = {
     characters: [],
   }
 
+  componentDidMount() {
+   axios.get('http://localhost:5000/users')
+    .then(res => {
+      const characters = res.data.users_list;
+      this.setState({ characters });
+    })
+    .catch(function (error) {
+      //Not handling the error. Just logging into the console.
+      console.log(error);
+    });
+  }
 
   removeCharacter = index => {
     const {characters } = this.state
