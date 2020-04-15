26888673bd395f7da0d3c562feb419424e2d1125
Taylor Hayase <thayase@calpoly.edu>
2020-04-15 14:15:25 -0700

    starting to add delete

--- src/App.js
+++ src/App.js
@@ -24,7 +24,21 @@ class App extends Component {
    return axios.post('http://localhost:5000/users', character)
     .then(function (response) {
       console.log(response);
-      return (response.status === 201);
+      return (response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+      return false;
+    });
+  }	
+
+  makeDeleteCall(character){
+  	var url = 'http://localhost:5000/users'
+  	console.log(url.concat('/', character.id))
+   	return axios.delete(url.concat('/', character.id), character)
+    .then(function (response) {
+      console.log(response);
+      return (true);
     })
     .catch(function (error) {
       console.log(error);
@@ -35,18 +49,14 @@ class App extends Component {
   removeCharacter = index => {
     const {characters } = this.state
 
-    this.setState({
-      characters: characters.filter((character, i) => {
-        return i !== index
-      }),
-    })
+    this.makeDeleteCall(characters[index]).then (callResult => {
+    	this.setState({ characters : [callResult] });
+    });
   }
 
   handleSubmit = character => {
     this.makePostCall(character).then( callResult => {
-    	if (callResult === true) {
-        	this.setState({ characters: [...this.state.characters, character] });
-      	}
+        this.setState({ characters: [...this.state.characters, callResult] });
    	});
   }
 
